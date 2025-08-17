3da95b003172b076ac350d433cfa7e410750900a
muhammad-adnan <mohammadadnanwasti@gmail.com>
2025-08-18 00:59:24 +0600

    Overview updated , Alhamdulillah

--- src/pages/DashBoard/Overview.jsx
+++ src/pages/DashBoard/Overview.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import useCourts from "../hooks/adminCourts/useCourts";
 
 const Overview = () => {
-    
+  const { data: courts = [] } = useCourts();
+  console.log(courts);
+
   return (
-    <div>
-     
-hfghfghfg
-
+    <div className="w-full h-96 bg-base-100 p-4 shadow rounded-xl">
+      <h2 className="text-xl font-bold mb-4">Court Pricing Overview</h2>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart data={courts}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="type" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          {/* Show court prices */}
+          <Bar dataKey="price" fill="#76b38f" />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
-  )
-}
+  );
+};
 
-export default Overview
+export default Overview;
